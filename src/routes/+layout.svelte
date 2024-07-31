24e1d5c284dd7b330060cb6f7668f47bbe1a2e66
ada <cmdada@proton.me>
2024-07-30 19:35:57 -0600

    okay almost done with the art page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -72,6 +72,7 @@
         </div>
         <div id="nav">
             <a href="/" class:current={$page.url.pathname == "/"}>~/Home</a>
+            <a href="/art" class:current={$page.url.pathname == "/art"}>~/Art</a>
             <a href="/services" class:current={$page.url.pathname == "/services"}>~/Services</a>
         </div>
         <hr />
